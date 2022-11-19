dfced67291529d8e30b36adaa7d77746c011ee50
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-11-19 14:06:51 -0500

    signed-mod

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,30 @@
 import randomInRange from './randomInRange';
+import signedMod from './signedMod';
 
 
 interface HedgehogInterface {
     randomInRange: (min: number, max: number) => number
+	signedMod: (num: number, modulator: number) => number
 }
 
 export const hedgehog: HedgehogInterface = {
 	randomInRange: (min: number, max: number) => {
 		return randomInRange(min, max);
+	},
+	signedMod: (num: number, modulator: number): number => {
+		return signedMod(num, modulator);
 	}
 };
 
 export class Hedgehog implements HedgehogInterface {
 	randomInRange!: (min: number, max: number) => number;
+	signedMod!: (num: number, modulator: number) => number;
 	constructor() {
 		this.randomInRange = (min: number, max: number) => {
 			return randomInRange(min, max);
+		};
+		this.signedMod = (num: number, modulator: number) => {
+			return signedMod(num, modulator);
 		};
 	}
 }
