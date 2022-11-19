c3904adfd2d8a1e7ee93d576b81d8776d3bb7074
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-11-19 13:42:26 -0500

    example helper function @HedgehogDubz

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
+import randomInRange from './randomInRange';
+
+
 interface HedgehogInterface {
-    
+    randomInRange: (min: number, max: number) => number
 }
 
-const hedgehog = {
-
+export const hedgehog: HedgehogInterface = {
+	randomInRange: (min: number, max: number) => {
+		return randomInRange(min, max);
+	}
 };
 
-class Hedgehog {
-
+export class Hedgehog implements HedgehogInterface {
+	randomInRange!: (min: number, max: number) => number;
+	constructor() {
+		this.randomInRange = (min: number, max: number) => {
+			return randomInRange(min, max);
+		};
+	}
 }
---
+++ src/randomInRange.ts
@@ -0,0 +1,1 @@
+export default function randomInRange(min: number, max: number): number { return Math.round(Math.random() * (max - min) + min); }
