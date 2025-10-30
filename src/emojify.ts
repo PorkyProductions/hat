fe1cb84545698327ca8da0e21f6fca53c97c5f3e
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2025-10-30 13:21:47 -0400

    refactor types

--- src/emojify.ts
+++ src/emojify.ts
@@ -22,9 +22,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-interface EmojiDictionary {
-	[key: string]: string;
-}
+
+import { EmojiDictionary } from "./types";
 
 const emojiDict: EmojiDictionary = {
 	a: '🐶',
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
 export type Maybe<T> = T | null | undefined;
 export type MaybeArray<T> = T[] | null | undefined;
 export type Matrix = number[][];
+export interface EmojiDictionary {
+	[key: string]: string;
+}
